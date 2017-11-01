7d847c70aa68dc072e7678bbc9b06a1ccd7019c7
Shilpa Jain02 <Shilpa_Jain02@ad.infosys.com>
2017-11-01 16:32:00 +0530

    code commit

---
+++ src/NewTest.java
@@ -0,0 +1,27 @@
+import org.openqa.selenium.By;
+ 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class NewTest {
+	
+	public WebDriver driver;
+	
+	@Test
+  public void main()
+  {
+	  System.setProperty("webdriver.chrome.driver","D:\\ChromeWebDriver\\chromedriver.exe"); //Set a system property “webdriver.chrome.driver” to the path of your ChromeDriver.exe file
+	  
+      WebDriver driver = new ChromeDriver(); //instantiate a driver object in Selenium
+      //WebDriverWait waiting = new WebDriverWait(driver, 10, 100);
+      
+      driver.get("https://facebook.com/");  //navigate to a page.
+      driver.findElement(By.id("email")).sendKeys("shilpa.jain812@gmail.com"); 
+      driver.findElement(By.id("pass")).sendKeys("Facebook@123");
+      
+      driver.findElement(By.id("loginbutton")).click();
+      System.out.println("Successfully logged in");
+ 
+  }
+}
