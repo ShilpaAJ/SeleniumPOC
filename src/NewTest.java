f93e70b719a704d95e7656b5caa343d64a41e5b0
GUPTA <ag081u@att.com>
2018-01-21 23:15:09 -0800

    adding updated

--- src/NewTest.java
+++ src/NewTest.java
@@ -1,8 +1,12 @@
 import org.openqa.selenium.By;
 import com.beust.jcommander.JCommander;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 //import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
 import com.beust.jcommander.Parameter;
 import org.testng.TestNG;
@@ -12,21 +16,31 @@ public class NewTest {
 	public WebDriver driver;
 	
 	@Test
-  public void main()
+  public void main() throws InterruptedException
   {
-	  //System.out.println("setting /usr/local/share/chromedriver");
-	  //System.setProperty("webdriver.chrome.driver","/usr/local/share/chromedriver");
+	  System.out.println("setting chromedriver");
+	  System.setProperty("webdriver.chrome.driver","C:/Users/ag081u/Documents/aalok/softwares/chromedriver.exe");
       System.out.println("About to Initialize");
-      WebDriver driver = new FirefoxDriver(); //instantiate a driver object in Selenium
+      ChromeOptions options = new ChromeOptions();
+      options.addArguments("--disable-notifications");
+      WebDriver driver = new ChromeDriver(options); //instantiate a driver object in Selenium
+
+
       System.out.println("Initialized");
       //WebDriverWait waiting = new WebDriverWait(driver, 10, 100);
       
       driver.get("https://facebook.com/");  //navigate to a page.
-      driver.findElement(By.id("email")).sendKeys("shamit619"); 
-      driver.findElement(By.id("pass")).sendKeys("pastasalsa22");
+      driver.findElement(By.id("email")).sendKeys("shilpa.jain812@gmail.com"); 
+      driver.findElement(By.id("pass")).sendKeys("Facebook@123");
       
       driver.findElement(By.id("loginbutton")).click();
       System.out.println("Successfully logged in");
- 
+     // driver.findElement(By.partialLinkText("on your mind, Aalok?")).click();
+      Thread.sleep(2000);
+      driver.findElement(By.xpath("//span[contains(text(), 'Make Post')]")).click();
+      Thread.sleep(2000);
+      driver.findElement(By.xpath("//span[contains(text(), 'Post')]")).click();
+
+      
   }
 }
