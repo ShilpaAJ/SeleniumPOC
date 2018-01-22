a8565cf1f73aae5b98a8d5bad38827570b3ac5ce
GUPTA <ag081u@att.com>
2018-01-22 10:12:22 -0800

    adding updated

--- src/NewTest.java
+++ src/NewTest.java
@@ -1,23 +1,15 @@
 import java.io.File;
 import java.io.IOException;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
-import java.io.File;
-import org.apache.commons.io.FileUtils;
-import com.beust.jcommander.JCommander;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-//import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxProfile;
-import org.openqa.selenium.interactions.Actions;
+//import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
-import com.beust.jcommander.Parameter;
-import org.testng.TestNG;
 
 public class NewTest {
 	
@@ -26,47 +18,39 @@ public class NewTest {
 	@Test
   public void main() throws InterruptedException, IOException
   {
-	  System.out.println("setting chromedriver");
-	  System.setProperty("webdriver.chrome.driver","C:/Users/ag081u/Documents/aalok/softwares/chromedriver.exe");
-      System.out.println("About to Initialize");
-      ChromeOptions options = new ChromeOptions();
-      options.addArguments("--disable-notifications");
-      
-   	  FirefoxProfile ffprofile = new FirefoxProfile();
-  	  ffprofile.setPreference("dom.webnotifications.enabled", false);
-  	  WebDriver driver = new FirefoxDriver(ffprofile);
-     // WebDriver driver = new ChromeDriver(options); //instantiate a driver object in Selenium
-     // WebDriver driver = new FirefoxDriver(); //instantiate a driver object in Selenium
-
-
-      System.out.println("Initialized");
+	  System.setProperty("webdriver.chrome.driver","D:\\ChromeWebDriver\\chromedriver.exe"); //Set a system property “webdriver.chrome.driver” to the path of your ChromeDriver.exe file
+	  //instantiate a driver object in Selenium
+      WebDriver driver = new ChromeDriver(); 
+      driver.manage().window().maximize();
       //WebDriverWait waiting = new WebDriverWait(driver, 10, 100);
       
-      driver.get("https://facebook.com/");  //navigate to a page.
-      driver.findElement(By.id("email")).sendKeys("shilpa.jain812@gmail.com"); 
-      driver.findElement(By.id("pass")).sendKeys("Facebook@123");
+      String strControl1 = "//div[contains(@class, 'tup')]//a[contains(text(), 'Movies')]";
+      String strControl2 = "//div[contains(@class, 'page-module-content')]//div[contains(@class, 'Carousel')][1]//li[1]//a[contains(@class, 'flip-title')]";
       
-      driver.findElement(By.id("loginbutton")).click();
-      System.out.println("Successfully logged in");
-      
-      Thread.sleep(2000);
+      //driver.get("https://facebook.com/");  //navigate to a page.
+      driver.get("https://www.directv.com/entertainment");
+      Thread.sleep(5000);
+      driver.findElement(By.xpath(strControl1)).click();
+      Thread.sleep(5000);
       System.out.println("Before capture & driver is " + driver);
       File screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-      System.out.println("line 553, time is " + System.currentTimeMillis());
-      //FileUtils.copyFile(screenshot, new File("/home/jenkins/sharedspace/TO_BE_Deleted_sk8457/selenium_chrome/src/Results/Reports/screenshot" + System.currentTimeMillis() + ".png"));
-      FileUtils.copyFile(screenshot, new File("/home/jenkins/sharedspace/screenshot" + System.currentTimeMillis() + ".png"));
-      System.out.println("Screenshot captured");
-      driver.findElement(By.xpath("//span[contains(text(), 'Make Post')]")).click();
-      Thread.sleep(2000);
-      File screenshot1 = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+      System.out.println("After Movie click, time is " +  System.currentTimeMillis());
+      FileUtils.copyFile(screenshot, new File("D:\\ChromeWebDriver\\screenshot" + System.currentTimeMillis() + ".png"));
+      System.out.println("Screenshot1 captured");
 
-      FileUtils.copyFile(screenshot1, new File("/home/jenkins/sharedspace/screenshot" + System.currentTimeMillis() + ".png"));
-
-      driver.findElement(By.xpath("//span[contains(text(), 'Post')]")).click();
-      File screenshot2 = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-
-      FileUtils.copyFile(screenshot2, new File("/home/jenkins/sharedspace/screenshot" + System.currentTimeMillis() + ".png"));
-
+      driver.findElement(By.xpath(strControl2)).click();
+      Thread.sleep(5000);
       
+      File screenshot1 = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+      System.out.println("After movie elemet click, time is " +  System.currentTimeMillis());
+      FileUtils.copyFile(screenshot1, new File("D:\\ChromeWebDriver\\screenshot" + System.currentTimeMillis() + ".png"));
+      System.out.println("Screenshot2 captured");
+      
+      //driver.findElement(By.id("email")).sendKeys("shilpa.jain812@gmail.com"); 
+      //driver.findElement(By.id("pass")).sendKeys("Facebook@123");
+      
+      //driver.findElement(By.id("loginbutton")).click();
+      System.out.println("Successfully logged in");
+ 
   }
 }
